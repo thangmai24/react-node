1a7957149cb222c1a974aa6b2e17597f3da8f52e
thangmai24 <thangmai0107@gmail.com>
2025-10-16 01:38:07 +0700

    test ver1.1.6

--- nodejs-enghlish/nodejs-enghlish/goiy/server/src/server.js
+++ nodejs-enghlish/nodejs-enghlish/goiy/server/src/server.js
@@ -1,4 +1,6 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
+console.log("CORS_ORIGIN:", process.env.CORS_ORIGIN);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
